6d187ca8295daf0dc851c71864ba5158d5cf0d7b
Derrick Lambert <drekryan@yahoo.com>
2017-02-23 19:00:28 -0500

    Remove Full Notify

--- src/main/java/com/philderbeast/prisonpicks/PrisonPicks.java
+++ src/main/java/com/philderbeast/prisonpicks/PrisonPicks.java
@@ -3,10 +3,7 @@ package com.philderbeast.prisonpicks;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
-import java.util.ArrayList;
 import java.util.UUID;
-import com.philderbeast.prisonpicks.Events;
-import com.philderbeast.prisonpicks.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -19,13 +16,12 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PrisonPicks extends JavaPlugin {
-    ArrayList<String> disabledAlert = new ArrayList<>();
 
     private static PrisonPicks instance;
 
     public void onEnable() {
         instance = this;
-        this.getServer().getPluginManager().registerEvents(new Events(this), (Plugin)this);
+        this.getServer().getPluginManager().registerEvents(new Events(this),this);
     }
 
     public void onDisable() {
@@ -129,24 +125,8 @@ public class PrisonPicks extends JavaPlugin {
                 sender.sendMessage(ChatColor.RED + "Usage: /pick [type] [player]");
             }
         }
-
-        if (label.equalsIgnoreCase("fullnotify")) {
-            if (sender instanceof Player) {
-                player = (Player)sender;
-                if (disabledAlert.contains(player.getName())) {
-                    player.sendMessage(ChatColor.GREEN + "Inventory Full Notifications have been ENABLED");
-                    disabledAlert.remove(player.getName());
-                } else {
-                    player.sendMessage(ChatColor.RED + "Inventory Full Notifications have been DISABLED");
-                    disabledAlert.add(player.getName());
-                }
-                return true;
-            } else {
-                sender.sendMessage("Sorry, that command is ingame only!");
-            }
-        }
         
-    return false;
+        return false;
     }
 
     public static WorldGuardPlugin getWorldGuard() {
@@ -165,9 +145,5 @@ public class PrisonPicks extends JavaPlugin {
             return true;
         }
         return false;
-    }
-
-    ArrayList<String> getDisabledAlerts() {
-        return disabledAlert;
     }
 }
